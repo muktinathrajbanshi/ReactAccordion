b5f116c47f94e3402ec8f0035629c0dfeb23bc31
unknown <muktinathrajbanshi786@gmail.com>
2025-11-04 20:04:05 +0545

    wow now it's perfect

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,11 +1,29 @@
 import { useEffect, useState } from "react";
-import {FAQ} from "../API/faq.json";
+import faq from "../API/faq.json";
 
 export const Accordion = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        setData(FAQ);
+        setData(faq);
     }, []);
-    return <h1>Hello React!</h1>;
+    return (
+        <>
+            <h1>The Accordion</h1>
+            <ul className="section-accordion">
+                {
+                    data.map((curElem) => {
+                        return (
+                            <li key={curElem.id}>
+                                <div className="accordion-grid">
+                                    <p>{curElem.question}</p>
+                                    <button>Show</button>
+                                </div>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </>
+    );
 };
