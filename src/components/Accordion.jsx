d3140edcdacb0a063bcb3cb977a16e4b61ea5fe2
unknown <muktinathrajbanshi786@gmail.com>
2025-11-04 19:58:01 +0545

    wow now it's time for using map method for fetching data

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from "react";
+import {FAQ} from "../API/faq.json";
+
 export const Accordion = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        setData(FAQ);
+    }, []);
     return <h1>Hello React!</h1>;
 };
